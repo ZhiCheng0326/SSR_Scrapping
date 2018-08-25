0c3958aad90ee5917b7bff0acd7297b313d2f9a0
Zhi Cheng Lee <leezhixin2001@gmail.com>
2018-08-26 00:07:19 +0800

    config.py added

--- src/main.py
+++ src/main.py
@@ -8,9 +8,10 @@ def main():
     
     SSR = getSSRstring(str_SSRaddress)
     config = decoder(SSR)
-    print(config)
-
+    updateConfig(config)
+ 
 if __name__ == "__main__":
     from ssr_scrapping import*
     from decoder import decoder
+    from update import updateConfig
     main()
