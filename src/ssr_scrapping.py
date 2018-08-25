2e3dc24abc64d804864bb415ac52d12e1020ab8a
Zhi Cheng Lee <leezhixin2001@gmail.com>
2018-08-25 18:09:30 +0800

    Create ssr_scrapping.py

---
+++ src/ssr_scrapping.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import bs4
+import requests
+
+def getPage():
+    url = "https://fangeqiang.com/408.html"
+    page = requests.get(url)    
+    soup = bs4.BeautifulSoup(page.content, 'lxml')
+    return soup
+
+
+#extract the ssr address out of the other VPN addresses
+def getSSRdiv(soup):
+    Alladdress = soup.find_all('div',attrs={'class':'xContent'})
+    
+    for address in Alladdress:
+        if 'ssr' in address.text:
+            return address.pre.text     #convert into string format
+    
+    if SSRaddress == None:
+        return None
+
+    
+#extract the ssr address
+def getSSRstring(str_SSRaddress):
+    keyword = 'ssr://'
+    index = str_SSRaddress.find(keyword)
+    addressList = []
+    
+    while str_SSRaddress[index] != '\r':
+        addressList.append(str_SSRaddress[index])
+        index += 1
+    
+    SSR = "".join(addressList)
+    return SSR
